nicolasenno/ack-networkfirewall-controller

Add logging configuration to firewall
Issue (aws-controllers-k8s/community#1553)

Description of changes:
Adds support for logging configuration to firwall spec

By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

apis/v1alpha1/zz_generated.deepcopy.go
@@ -674,6 +674,11 @@ func (in *FirewallSpec) DeepCopyInto(out *FirewallSpec) {
 		*out = new(bool)
 		**out = **in
 	}
+	if in.LoggingConfiguration != nil {
+		in, out := &in.LoggingConfiguration, &out.LoggingConfiguration
+		*out = new(LoggingConfiguration)
+		(*in).DeepCopyInto(*out)
+	}
 	if in.SubnetChangeProtection != nil {
 		in, out := &in.SubnetChangeProtection, &out.SubnetChangeProtection
 		*out = new(bool)
@@ -998,6 +1003,72 @@ func (in *IPSetReference) DeepCopy() *IPSetReference {
 		return nil
 	}
 	out := new(IPSetReference)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *LogDestinationConfig) DeepCopyInto(out *LogDestinationConfig) {
+	*out = *in
+	if in.LogDestination != nil {
+		in, out := &in.LogDestination, &out.LogDestination
+		*out = make(map[string]*string, len(*in))
+		for key, val := range *in {
+			var outVal *string
+			if val == nil {
+				(*out)[key] = nil
+			} else {
+				in, out := &val, &outVal
+				*out = new(string)
+				**out = **in
+			}
+			(*out)[key] = outVal
+		}
+	}
+	if in.LogDestinationType != nil {
+		in, out := &in.LogDestinationType, &out.LogDestinationType
+		*out = new(string)
+		**out = **in
+	}
+	if in.LogType != nil {
+		in, out := &in.LogType, &out.LogType
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new LogDestinationConfig.
+func (in *LogDestinationConfig) DeepCopy() *LogDestinationConfig {
+	if in == nil {
+		return nil
+	}
+	out := new(LogDestinationConfig)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *LoggingConfiguration) DeepCopyInto(out *LoggingConfiguration) {
+	*out = *in
+	if in.LogDestinationConfigs != nil {
+		in, out := &in.LogDestinationConfigs, &out.LogDestinationConfigs
+		*out = make([]*LogDestinationConfig, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(LogDestinationConfig)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new LoggingConfiguration.
+func (in *LoggingConfiguration) DeepCopy() *LoggingConfiguration {
+	if in == nil {
+		return nil
+	}
+	out := new(LoggingConfiguration)
 	in.DeepCopyInto(out)
 	return out
 }

aws-controllers-k8s/networkfirewall-controller

apis/v1alpha1/zz_generated.deepcopy.go
@@ -725,6 +725,11 @@ func (in *FirewallSpec) DeepCopyInto(out *FirewallSpec) {
 		*out = new(bool)
 		**out = **in
 	}
+	if in.LoggingConfiguration != nil {
+		in, out := &in.LoggingConfiguration, &out.LoggingConfiguration
+		*out = new(LoggingConfiguration)
+		(*in).DeepCopyInto(*out)
+	}
 	if in.SubnetChangeProtection != nil {
 		in, out := &in.SubnetChangeProtection, &out.SubnetChangeProtection
 		*out = new(bool)
@@ -1050,6 +1055,73 @@ func (in *IPSetReference) DeepCopy() *IPSetReference {
 		return nil
 	}
 	out := new(IPSetReference)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *LogDestinationConfig) DeepCopyInto(out *LogDestinationConfig) {
+	*out = *in
+	if in.LogDestination != nil {
+		in, out := &in.LogDestination, &out.LogDestination
+		*out = make(map[string]*string, len(*in))
+		for key, val := range *in {
+			var outVal *string
+			if val == nil {
+				(*out)[key] = nil
+			} else {
+				inVal := (*in)[key]
+				in, out := &inVal, &outVal
+				*out = new(string)
+				**out = **in
+			}
+			(*out)[key] = outVal
+		}
+	}
+	if in.LogDestinationType != nil {
+		in, out := &in.LogDestinationType, &out.LogDestinationType
+		*out = new(string)
+		**out = **in
+	}
+	if in.LogType != nil {
+		in, out := &in.LogType, &out.LogType
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new LogDestinationConfig.
+func (in *LogDestinationConfig) DeepCopy() *LogDestinationConfig {
+	if in == nil {
+		return nil
+	}
+	out := new(LogDestinationConfig)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *LoggingConfiguration) DeepCopyInto(out *LoggingConfiguration) {
+	*out = *in
+	if in.LogDestinationConfigs != nil {
+		in, out := &in.LogDestinationConfigs, &out.LogDestinationConfigs
+		*out = make([]*LogDestinationConfig, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(LogDestinationConfig)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new LoggingConfiguration.
+func (in *LoggingConfiguration) DeepCopy() *LoggingConfiguration {
+	if in == nil {
+		return nil
+	}
+	out := new(LoggingConfiguration)
 	in.DeepCopyInto(out)
 	return out
 }