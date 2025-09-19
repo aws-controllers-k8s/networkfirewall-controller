629efa5295e3bff8843e60a1bc1c5bee5fbbf536
ci-robot <ci-robot@k8s.io>
2025-09-19 17:22:13 +0000

    Update to ACK runtime `v0.52.0`, code-generator `v0.52.0`

--- pkg/resource/firewall_policy/resource.go
+++ pkg/resource/firewall_policy/resource.go
@@ -97,11 +97,11 @@ func (r *resource) SetIdentifiers(identifier *ackv1alpha1.AWSIdentifiers) error 
 
 // PopulateResourceFromAnnotation populates the fields passed from adoption annotation
 func (r *resource) PopulateResourceFromAnnotation(fields map[string]string) error {
-	tmp, ok := fields["firewallPolicyName"]
+	primaryKey, ok := fields["firewallPolicyName"]
 	if !ok {
 		return ackerrors.NewTerminalError(fmt.Errorf("required field missing: firewallPolicyName"))
 	}
-	r.ko.Spec.FirewallPolicyName = &tmp
+	r.ko.Spec.FirewallPolicyName = &primaryKey
 
 	return nil
 }
